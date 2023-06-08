ea72c5d13058c82001fc518a069d9a411466d62c
sakib5452 <najmus191@gmail.com>
2023-06-08 07:43:07 +0600

    navber

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Pages/Shared/Navber/Navbar";
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Main;
